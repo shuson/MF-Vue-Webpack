3ce1dc88994d3e5fc29bd4f22d30a9a0c2b7c0ef
nevermoi <wlgq@hotmail.com>
2015-12-05 20:26:58 +0800

    add ajax loader css

--- src/components/App.vue
+++ src/components/App.vue
@@ -6,6 +6,18 @@
                 <a class="navbar-brand" href="#/">Admin Panel</a>
             </div>
             <!-- Top Menu Items -->
+            <div id="ajaxLoader" style="display:none">
+                <div id="fountainG">
+                    <div id="fountainG_1" class="fountainG"></div>
+                    <div id="fountainG_2" class="fountainG"></div>
+                    <div id="fountainG_3" class="fountainG"></div>
+                    <div id="fountainG_4" class="fountainG"></div>
+                    <div id="fountainG_5" class="fountainG"></div>
+                    <div id="fountainG_6" class="fountainG"></div>
+                    <div id="fountainG_7" class="fountainG"></div>
+                    <div id="fountainG_8" class="fountainG"></div>
+                </div>
+            </div>
             <ul class="nav navbar-right top-nav">
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> {{user.name}} <b class="caret"></b></a>
