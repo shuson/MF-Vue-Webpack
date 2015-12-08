8787080f7b745bd21f8ea5f6c8d8040b1c254a47
nevermoi <wlgq@hotmail.com>
2015-12-08 23:47:14 +0800

    add basic transaction detail page

--- src/components/TransactionView.vue
+++ src/components/TransactionView.vue
@@ -1,9 +1,62 @@
 <template>
 	<a href="javascript:window.history.back();"><i class="fa fa-arrow-left"></i> Back</a>
 	<div id="transactionView">
-		Transaction price is: {{transaction.price}}
-        <br />
-        Transaction buyer is {{transaction.buyer.firstname}}
+		<div class="row">
+			<div class="col-xs-12">
+				<h1 class="page-header">Transaction Details</h1>
+			</div>
+		</div>
+        <div class="row">
+            <div class="col-xs-12">
+                <div class="text-center">
+                    <h4>Reference Number: {{transaction.id}}</h4>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-xs-12 col-md-6 col-lg-4 pull-left">
+                        <div class="panel panel-info ">
+                            <div class="panel-heading">Seller Details</div>
+                            <div class="panel-body">
+                                <strong>Seller Name:</strong>
+                                {{transaction.seller.firstname}} {{transaction.seller.lastname}}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-6 col-lg-4">
+                        <div class="panel panel-info ">
+                            <div class="panel-heading">Buyer Details</div>
+                            <div class="panel-body">
+                                <strong>Buyer Name:</strong>
+                                {{transaction.buyer.firstname}} {{transaction.buyer.lastname}}
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-6 col-lg-4">
+                        <div class="panel panel-default height">
+                            <div class="panel-heading">Flight Details</div>
+                            <div class="panel-body">
+                                <strong>Departure Airport:</strong> {{transaction.departureAirport.airportName}} <br />
+                                <strong>Arrival Airport:</strong> {{transaction.arrivalAirport.airportName}} <br />
+                                <strong>Airline:</strong> {{transaction.airline.airlineName}}<br>
+                                <strong>Cabin Class:</strong> {{transaction.cabinClass}}<br>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-6 col-lg-4 pull-right">
+                        <div class="panel panel-default height">
+                            <div class="panel-heading">Shipping Address</div>
+                            <div class="panel-body">
+                                <strong>David Peere:</strong><br>
+                                1111 Army Navy Drive<br>
+                                Arlington<br>
+                                VA<br>
+                                <strong>22 203</strong><br>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 	</div>
 </template>
 <script>
@@ -32,5 +85,7 @@ export default {
 #transactionView{
 
 }
-
+.height {
+    min-height: 200px;
+}
 </style>
--- src/components/UserView.vue
+++ src/components/UserView.vue
@@ -9,46 +9,47 @@
 		<div class="row">
 			<div class="col-xs-12">
 				 <div class="panel panel-info">
-            <div class="panel-heading">
-              <h3 class="panel-title">{{customer.firstname}} {{customer.lastname}}</h3>
-            </div>
-            <div class="panel-body">
-              <div class="row">
-                <div class="col-md-3 col-lg-3 " align="center"> 
-                	<img alt="" :src="customer.avatar_url" class="img-circle img-responsive" />
+                    <div class="panel-heading">
+                        <h3 class="panel-title">{{customer.firstname}} {{customer.lastname}}</h3>
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="col-md-3 col-lg-3 " align="center"> 
+                	           <img alt="" :src="customer.avatar_url" class="img-circle img-responsive" />
+                            </div>
+                            <div class=" col-md-9 col-lg-9 "> 
+                              <table class="table table-user-information">
+                                <tbody>
+                                   <tr>
+                                    <td>Birthday:</td>
+                                    <td>{{customer.birthday}}</td>
+                                  </tr>
+                                  <tr>
+                                    <td>Country:</td>
+                                    <td>{{customer.country}}</td>
+                                  </tr>
+                                  <tr>
+                                    <td>City:</td>
+                                    <td>{{customer.addr_city}}</td>
+                                  </tr>
+                                  <tr>
+                                    <td>Languages:</td>
+                                    <td>{{customer.languages}}</td>
+                                  </tr>
+                                  <tr>
+                                    <td>Phone No.:</td>
+                                    <td>{{customer.phoneNo}}</td>
+                                  </tr>
+                                </tbody>
+                              </table>
+                              <a v-link="{path: '/users/' + customer.user_id + '/transactions'}" href="javascript:void(0)" class="btn btn-primary">View Transactions</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class=" col-md-9 col-lg-9 "> 
-                  <table class="table table-user-information">
-                    <tbody>
-                       <tr>
-                        <td>Birthday:</td>
-                        <td>{{customer.birthday}}</td>
-                      </tr>
-                      <tr>
-                        <td>Country:</td>
-                        <td>{{customer.country}}</td>
-                      </tr>
-                      <tr>
-                        <td>City:</td>
-                        <td>{{customer.addr_city}}</td>
-                      </tr>
-                      <tr>
-                        <td>Languages:</td>
-                        <td>{{customer.languages}}</td>
-                      </tr>
-                      <tr>
-                        <td>Phone No.:</td>
-                        <td>{{customer.phoneNo}}</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                  <a v-link="{path: '/users/' + customer.user_id + '/transactions'}" href="javascript:void(0)" class="btn btn-primary">View Transactions</a>
-                </div>
-              </div>
             </div>
-          </div>
-        </div>
-	</div>
+	   </div>
+    </div>
 </template>
 
 <script>
