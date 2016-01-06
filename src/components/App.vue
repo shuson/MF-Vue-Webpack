07b88d5673631e560aece16d92e95924e994898a
nevermoi <wlgq@hotmail.com>
2016-01-06 21:47:21 +0800

    add new items of home page

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,6 @@
 <template>
-	<div id="wrapper">
-		<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div id="wrapper">
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
                 <a class="navbar-brand" href="#/">Admin Panel</a>
@@ -22,7 +22,7 @@
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="min-width:200px"><i class="fa fa-user"></i> {{user.name}} <b class="caret"></b></a>
                     <ul class="dropdown-menu">
-												<li>
+                        <li>
                             <a href="/password/change"><i class="fa fa-fw fa-key"></i> Change Password</a>
                         </li>
                         <li>
@@ -38,190 +38,196 @@
                         <a v-link="{ path: '/'}" name="dashboard"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
                     <li @click="tabSwitch" :class="{'active': tabName == 'users'}">
-                        <a v-link="{ path: '/users?page=1'}" name="users"><i class="fa fa-fw fa-users"></i> Users</a>
+                        <a v-link="{ path: '/users?page=1'}" name="users"><i class="fa fa-fw fa-users"></i> Members</a>
                     </li>
                     <li @click="tabSwitch" :class="{'active': tabName == 'transactions'}">
-                        <a v-link="{ path: '/transactions?page=1'}" name="transactions"><i class="fa fa-fw fa-exchange"></i> Transactions</a>
+                        <a v-link="{ path: '/transactions?page=1'}" name="transactions"><i class="fa fa-fw fa-exchange"></i> Orders</a>
+                    </li>
+                    <li @click="tabSwitch" :class="{'active': tabName == 'pages'}">
+                        <a v-link="{ path: '/pages?page=1'}" name="pages"><i class="fa fa-fw fa-file"></i> Pages</a>
+                    </li>
+                    <li @click="tabSwitch" :class="{'active': tabName == 'inventory'}">
+                        <a v-link="{ path: '/inventory?page=1'}" name="inventory"><i class="fa fa-fw fa-list-alt"></i> Inventory</a>
+                    </li>
+                    <li @click="tabSwitch" :class="{'active': tabName == 'marketing'}">
+                        <a v-link="{ path: '/marketing?page=1'}" name="marketing"><i class="fa fa-fw fa-globe"></i> Marketing</a>
+                    </li>
+                    <li @click="tabSwitch" :class="{'active': tabName == 'promocode'}">
+                        <a v-link="{ path: '/promocode?page=1'}" name="promocode"><i class="fa fa-fw fa-pencil-square-o"></i> Discount / Promo Code</a>
+                    </li>
+                    <li @click="tabSwitch" :class="{'active': tabName == 'livechat'}">
+                        <a v-link="{ path: '/livechat'}" name="livechat"><i class="fa fa-fw fa-weixin"></i> Live Chat</a>
                     </li>
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
         </nav>
-         <!-- main view -->
-         <div id="page-wrapper">
-         	<router-view
-		      keep-alive
-			  >
-		    </router-view>
-         </div>
-	</div>
+        <!-- main view -->
+        <div id="page-wrapper">
+            <router-view keep-alive>
+            </router-view>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-    name: "app",
-	data () {
-		return {
-			user: pageObject.user,
-			tabName: pageObject.tabName,
-			
-		}
-	},
-	methods: {
-		tabSwitch: function(event){
-			this.tabName = event.target.name
-			pageObject.tabName = this.tabName
-		},
-	},
-	created () {
-    let hashIndex = location.href.lastIndexOf("#!") + 3
-    let searchIndex = location.href.lastIndexOf("?")
-		let tempTagName = location.href.substr(hashIndex, searchIndex-hashIndex) || "dashboard"
+    export default {
+        name: "app",
+        data() {
+            return {
+                user: pageObject.user,
+                tabName: pageObject.tabName,
 
-		this.tabName = tempTagName
-	}
-}
+            }
+        },
+        methods: {
+            tabSwitch: function (event) {
+                this.tabName = event.target.name
+                pageObject.tabName = this.tabName
+            },
+        },
+        created() {
+            let hashIndex = location.href.lastIndexOf("#!") + 3
+            let searchIndex = location.href.lastIndexOf("?")
+            let tempTagName = location.href.substr(hashIndex, searchIndex - hashIndex) || "dashboard"
+
+            this.tabName = tempTagName
+        }
+    }
 </script>
 
 <style>
-#wrapper {
-    padding-left: 0;
-}
-
-#page-wrapper {
-    width: 100%;
-    padding: 10px 50px;
-    height:100%; 
-    background-color: #fff;
-}
-
-@media(min-width:768px) {
     #wrapper {
-        padding-left: 225px;
+        padding-left: 0;
     }
 
     #page-wrapper {
+        width: 100%;
         padding: 10px 50px;
-        min-height: 400px;
+        height: 100%;
+        background-color: #fff;
     }
     
-    #ajaxLoader{
-        left: 50%
+    @media(min-width:768px) {
+        #wrapper {
+            padding-left: 225px;
+        }
+        #page-wrapper {
+            padding: 10px 50px;
+            min-height: 400px;
+        }
+        #ajaxLoader {
+            left: 50%
+        }
     }
-}
+    /* Top Navigation */
 
-/* Top Navigation */
+    .top-nav {
+        padding: 0 15px;
+    }
 
-.top-nav {
-    padding: 0 15px;
-}
+    .top-nav>li {
+        display: inline-block;
+        float: left;
+        min-width: 200px;
+    }
 
-.top-nav>li {
-    display: inline-block;
-    float: left;
-		min-width: 200px;
-}
+    .top-nav>li>a {
+        padding-top: 15px;
+        padding-bottom: 15px;
+        line-height: 20px;
+        color: #999;
+    }
 
-.top-nav>li>a {
-    padding-top: 15px;
-    padding-bottom: 15px;
-    line-height: 20px;
-    color: #999;
-}
+    .top-nav>li>a:hover,
+    .top-nav>li>a:focus,
+    .top-nav>.open>a,
+    .top-nav>.open>a:hover,
+    .top-nav>.open>a:focus {
+        color: #fff;
+        background-color: #000;
+    }
 
-.top-nav>li>a:hover,
-.top-nav>li>a:focus,
-.top-nav>.open>a,
-.top-nav>.open>a:hover,
-.top-nav>.open>a:focus {
-    color: #fff;
-    background-color: #000;
-}
+    .top-nav>.open>.dropdown-menu {
+        float: left;
+        position: absolute;
+        margin-top: 0;
+        min-width: 200px;
+        border: 1px solid rgba(0, 0, 0, .15);
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        background-color: #fff;
+        -webkit-box-shadow: 0 6px 12px rgba(0, 0, 0, .175);
+        box-shadow: 0 6px 12px rgba(0, 0, 0, .175);
+    }
 
-.top-nav>.open>.dropdown-menu {
-    float: left;
-    position: absolute;
-    margin-top: 0;
-		min-width: 200px;
-    border: 1px solid rgba(0,0,0,.15);
-    border-top-left-radius: 0;
-    border-top-right-radius: 0;
-    background-color: #fff;
-    -webkit-box-shadow: 0 6px 12px rgba(0,0,0,.175);
-    box-shadow: 0 6px 12px rgba(0,0,0,.175);
-}
+    .top-nav>.open>.dropdown-menu>li>a {
+        white-space: normal;
+    }
 
-.top-nav>.open>.dropdown-menu>li>a {
-    white-space: normal;
-}
-
-ul.message-dropdown {
-    padding: 0;
-    max-height: 250px;
-    overflow-x: hidden;
-    overflow-y: auto;
-}
-
-li.message-preview {
-    width: 275px;
-    border-bottom: 1px solid rgba(0,0,0,.15);
-}
-
-li.message-preview>a {
-    padding-top: 15px;
-    padding-bottom: 15px;
-}
-
-li.message-footer {
-    margin: 5px 0;
-}
-
-ul.alert-dropdown {
-    width: 200px;
-}
-
-/* Side Navigation */
-
-@media(min-width:768px) {
-    .side-nav {
-        position: fixed;
-        top: 51px;
-        left: 225px;
-        width: 225px;
-        margin-left: -225px;
-        border: none;
-        border-radius: 0;
-        overflow-y: auto;
-        background-color: #3F3E3F;
-        bottom: 0;
+    ul.message-dropdown {
+        padding: 0;
+        max-height: 250px;
         overflow-x: hidden;
-        padding-bottom: 40px;
+        overflow-y: auto;
     }
 
-    .side-nav>li>a {
-        width: 225px;
+    li.message-preview {
+        width: 275px;
+        border-bottom: 1px solid rgba(0, 0, 0, .15);
     }
 
-    .side-nav li a:hover,
-    .side-nav li a:focus {
-        outline: none;
-        background-color: #000 !important;
+    li.message-preview>a {
+        padding-top: 15px;
+        padding-bottom: 15px;
     }
-}
 
-.side-nav>li>ul {
-    padding: 0;
-}
+    li.message-footer {
+        margin: 5px 0;
+    }
 
-.side-nav>li>ul>li>a {
-    display: block;
-    padding: 10px 15px 10px 38px;
-    text-decoration: none;
-    color: #999;
-}
+    ul.alert-dropdown {
+        width: 200px;
+    }
+    /* Side Navigation */
 
-.side-nav>li>ul>li>a:hover {
-    color: #fff;
-}
+    @media(min-width:768px) {
+        .side-nav {
+            position: fixed;
+            top: 51px;
+            left: 225px;
+            width: 225px;
+            margin-left: -225px;
+            border: none;
+            border-radius: 0;
+            overflow-y: auto;
+            background-color: #3F3E3F;
+            bottom: 0;
+            overflow-x: hidden;
+            padding-bottom: 40px;
+        }
+        .side-nav>li>a {
+            width: 225px;
+        }
+        .side-nav li a:hover,
+        .side-nav li a:focus {
+            outline: none;
+            background-color: #000 !important;
+        }
+    }
 
+    .side-nav>li>ul {
+        padding: 0;
+    }
+
+    .side-nav>li>ul>li>a {
+        display: block;
+        padding: 10px 15px 10px 38px;
+        text-decoration: none;
+        color: #999;
+    }
+
+    .side-nav>li>ul>li>a:hover {
+        color: #fff;
+    }
 </style>
--- src/components/DashboardView.vue
+++ src/components/DashboardView.vue
@@ -1,154 +1,173 @@
 <template>
-	<div id="dashbaordView" class="container-fluid">
-	 	<div class="row">
-        <div class="col-lg-4 col-md-6">
-            <div class="panel panel-primary">
-                <div class="panel-heading">
-                    <div class="row">
-                        <div class="col-xs-3">
-                            <i class="fa fa-user-plus fa-5x"></i>
-                        </div>
-                        <div class="col-xs-9 text-right">
-                            <div class="huge">{{countUsersToday}}</div>
-                            <div>New users of today!</div>
+    <div id="dashbaordView" class="container-fluid">
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="panel panel-red">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <i class="fa fa-exclamation-circle fa-5x"></i>
+                            </div>
+                            <div class="col-xs-9 text-right">
+                                <div class="huge">0</div>
+                                <div>Notification</div>
+                            </div>
                         </div>
                     </div>
+                    <a href="#">
+                        <div class="panel-footer">
+                            <span class="pull-left">View Notifications</span>
+                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
                 </div>
-                <a v-link="{ path: '/newusers'}" name="users">
-                    <div class="panel-footer">
-                        <span class="pull-left">View Users</span>
-                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                        <div class="clearfix"></div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <i class="fa fa-user-plus fa-5x"></i>
+                            </div>
+                            <div class="col-xs-9 text-right">
+                                <div class="huge">{{countUsersToday}}</div>
+                                <div>New users of today!</div>
+                            </div>
+                        </div>
                     </div>
-                </a>
+                    <a v-link="{ path: '/newusers'}" name="users">
+                        <div class="panel-footer">
+                            <span class="pull-left">View Users</span>
+                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="panel panel-green">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <i class="fa fa-tasks fa-5x"></i>
+                            </div>
+                            <div class="col-xs-9 text-right">
+                                <div class="huge">{{countTransactionsToday}}</div>
+                                <div>New transactions of today!</div>
+                            </div>
+                        </div>
+                    </div>
+                    <a v-link="{ path: '/newtransactions'}" name="transactions">
+                        <div class="panel-footer">
+                            <span class="pull-left">View Transactions</span>
+                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="panel panel-yellow">
+                    <div class="panel-heading">
+                        <div class="row">
+                            <div class="col-xs-3">
+                                <i class="fa fa-list-alt fa-5x"></i>
+                            </div>
+                            <div class="col-xs-9 text-right">
+                                <div class="huge">0</div>
+                                <div>Inventory</div>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="#">
+                        <div class="panel-footer">
+                            <span class="pull-left">View Inventores</span>
+                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
+                </div>
             </div>
         </div>
-        <div class="col-lg-4 col-md-6">
-            <div class="panel panel-green">
-                <div class="panel-heading">
-                    <div class="row">
-                        <div class="col-xs-3">
-                            <i class="fa fa-tasks fa-5x"></i>
-                        </div>
-                        <div class="col-xs-9 text-right">
-                            <div class="huge">{{countTransactionsToday}}</div>
-                            <div>New transactions of today!</div>
-                        </div>
-                    </div>
-                </div>
-                <a v-link="{ path: '/newtransactions'}" name="transactions">
-                    <div class="panel-footer">
-                        <span class="pull-left">View Transactions</span>
-                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                        <div class="clearfix"></div>
-                    </div>
-                </a>
-            </div>
-        </div>
-        <div class="col-lg-4 col-md-6">
-            <div class="panel panel-yellow">
-                <div class="panel-heading">
-                    <div class="row">
-                        <div class="col-xs-3">
-                            <i class="fa fa-shopping-cart fa-5x"></i>
-                        </div>
-                        <div class="col-xs-9 text-right">
-                            <div class="huge">124</div>
-                            <div>New Orders!</div>
-                        </div>
-                    </div>
-                </div>
-                <a href="#">
-                    <div class="panel-footer">
-                        <span class="pull-left">View Details</span>
-                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                        <div class="clearfix"></div>
-                    </div>
-                </a>
-            </div>
-        </div>
-		</div>
-  </div>
+    </div>
 </template>
 
 <script>
-import store from "../store/dashboardStore"
+    import store from "../store/dashboardStore"
 
-export default {
-  name: 'DashboardView',
-  data () {
-		return {
-			countTransactionsToday: 0,
-			countUsersToday: 0,
-		}
-  },
-  
-  ready () {
-		store.countByToday().then(data => {
-			this.countTransactionsToday = data.countTransactions
-			this.countUsersToday = data.countUsers
-		})
-  }
-}
+    export default {
+        name: 'DashboardView',
+        data() {
+            return {
+                countTransactionsToday: 0,
+                countUsersToday: 0,
+            }
+        },
+
+        ready() {
+            store.countByToday().then(data => {
+                this.countTransactionsToday = data.countTransactions
+                this.countUsersToday = data.countUsers
+            })
+        }
+    }
 </script>
 
 <style>
-#dashbaordView{
+    #dashbaordView {}
     
-}
+    .panel-green {
+        border-color: #5cb85c;
+    }
 
-.panel-green {
-    border-color: #5cb85c;
-}
+    .panel-green > .panel-heading {
+        border-color: #5cb85c;
+        color: #fff;
+        background-color: #5cb85c;
+    }
 
-.panel-green > .panel-heading {
-    border-color: #5cb85c;
-    color: #fff;
-    background-color: #5cb85c;
-}
+    .panel-green > a {
+        color: #5cb85c;
+    }
 
-.panel-green > a {
-    color: #5cb85c;
-}
+    .panel-green > a:hover {
+        color: #3d8b3d;
+    }
 
-.panel-green > a:hover {
-    color: #3d8b3d;
-}
+    .panel-red {
+        border-color: #d9534f;
+    }
 
-.panel-red {
-    border-color: #d9534f;
-}
+    .panel-red > .panel-heading {
+        border-color: #d9534f;
+        color: #fff;
+        background-color: #d9534f;
+    }
 
-.panel-red > .panel-heading {
-    border-color: #d9534f;
-    color: #fff;
-    background-color: #d9534f;
-}
+    .panel-red > a {
+        color: #d9534f;
+    }
 
-.panel-red > a {
-    color: #d9534f;
-}
+    .panel-red > a:hover {
+        color: #b52b27;
+    }
 
-.panel-red > a:hover {
-    color: #b52b27;
-}
+    .panel-yellow {
+        border-color: #f0ad4e;
+    }
 
-.panel-yellow {
-    border-color: #f0ad4e;
-}
+    .panel-yellow > .panel-heading {
+        border-color: #f0ad4e;
+        color: #fff;
+        background-color: #f0ad4e;
+    }
 
-.panel-yellow > .panel-heading {
-    border-color: #f0ad4e;
-    color: #fff;
-    background-color: #f0ad4e;
-}
+    .panel-yellow > a {
+        color: #f0ad4e;
+    }
 
-.panel-yellow > a {
-    color: #f0ad4e;
-}
-
-.panel-yellow > a:hover {
-    color: #df8a13;
-}
-
+    .panel-yellow > a:hover {
+        color: #df8a13;
+    }
 </style>
